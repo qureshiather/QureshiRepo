f16f7ba069132e3721e4be6d1b504d304267e202
Ather Qureshi <aqures43@uwo.ca>
2016-11-30 20:06:24 -0500

    tested of ubuntu, made some hotfixes, and it works

--- Python Networking/UDP_Client.py
+++ Python Networking/UDP_Client.py
@@ -1,3 +1,4 @@
+# coding: utf-8
 import binascii
 import socket
 import struct
@@ -5,62 +6,61 @@ import sys
 import hashlib
 
 #given a packet, check if it's corrupt (using checkSum) 
-def checkCorrupt(bytes):
-    UDP_Packet = unpacker.unpack(bytes)
-    #Create the Checksum for comparison
-    values = (UDP_Packet[0],UDP_Packet[1],UDP_Packet[2])
-    packer = struct.Struct('I I 8s')
-    packed_data = packer.pack(*values)
-    chksum =  bytes(hashlib.md5(packed_data).hexdigest(), encoding="UTF-8")
-    #Compare Checksums to test for corrupt data
-    if UDP_Packet[3] == chksum:
-        return True
-    else:
-        return False
+def checkCorrupt(data):
+	UDP_Packet = unpacker.unpack(data)
+	#Create the Checksum for comparison
+	values = (UDP_Packet[0],UDP_Packet[1],UDP_Packet[2])
+	packer = struct.Struct('I I 8s')
+	packed_data = packer.pack(*values)
+	chksum =  bytes(hashlib.md5(packed_data).hexdigest())
+	#Compare Checksums to test for corrupt data
+	if UDP_Packet[3] == chksum:
+		return True
+	else:
+		return False
 
 #Checks the sequence "Bit" of a packet returns it
-def checkSequence(bytes):
-    UDP_Packet = unpacker.unpack(bytes)
-    return UDP_Packet[0]
+def checkSequence(data):
+	UDP_Packet = unpacker.unpack(data)
+	return UDP_Packet[0]
 
 #builds a pseudo UDP packet based on given Sequence number and String
-#ACK – Indicates if packet is ACK or not. Valid values (1 or 0)
-#SEQ – Sequence number of packet. Valid values (1 or 0)
+#ACK – Indicates if packet is ACK or not
+#SEQ – Sequence number of packet 
 #DATA – Application Data (8 bytes)
 #CHKSUM – MD5 Checksum of packet (32 Bytes)
-def buildPacket(str, seq):
-	values = (0, seq, str)
+def buildPacket(string, seq):
+	values = (0, seq, string)
 	UDP_Data = struct.Struct('I I 8s')
-	#ERROR RIGHT HERE
 	packed_data = UDP_Data.pack(*values)
 	#creates checksum based on values 
-	chksum =  bytes(hashlib.md5(packed_data).hexdigest(), encoding="UTF-8")
+	chksum = bytes(hashlib.md5(packed_data).hexdigest())
 	#now new value with chksum attached
-	values = (0, seq, str, chksum)
+	values = (0, seq, string, chksum)
 	UDP_Packet_Data = struct.Struct('I I 8s 32s')
 	return UDP_Packet_Data.pack(*values)
 
-def sendPacket(str, seq):
+def sendPacket(string, seq):
 	send_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 	recv_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 	
 	recv_sock.bind(us) #this socket waits for ACK packets
-	recv_sock.settimeout(9) #wait for 9 for ACK packet after recv is called
+	recv_sock.settimeout(9) #wait for 9s for ACK packet after recv is called
 
 	ack_received = False
 
 	while not ack_received:
-		packet = buildPacket(str, seq)
+		packet = buildPacket(string, seq)
 		send_sock.sendto(packet, dest) #Send DATA to server
 
 		try: 
 			data, addr = recv_sock.recvfrom(1024) # buffer size is 1024 bytes
-		except timeout:
+		except socket.timeout:
 			print ("Packet timed out")
 		else:
 			print (data)
 			ACK_seq = checkSequence(data)
-			if 	checkCorrupt(data) and ack_seq == seq:
+			if 	checkCorrupt(data) and ACK_seq == seq:
 				#break
 				ack_received = True
 
@@ -80,6 +80,7 @@ UDP_PORT = 4000
 
 us = (US_UDP_IP, US_UDP_PORT)
 dest = (UDP_IP, UDP_PORT)
+unpacker = struct.Struct('I I 8s 32s')
 
 print("UDP target IP:", UDP_IP)
 print("UDP target port:", UDP_PORT)
--- Python Networking/UDP_Server.py
+++ Python Networking/UDP_Server.py
@@ -1,3 +1,4 @@
+# coding: utf-8
 import binascii
 import socket
 import struct
@@ -6,40 +7,42 @@ import hashlib
 
 
 #will return true if not corrupt, and false if corrupt
-def checkCorrupt(bytes):
-    UDP_Packet = unpacker.unpack(bytes)
-    #Create the Checksum for comparison
-    values = (UDP_Packet[0],UDP_Packet[1],UDP_Packet[2])
-    packer = struct.Struct('I I 8s')
-    packed_data = packer.pack(*values)
-    chksum =  bytes(hashlib.md5(packed_data).hexdigest(), encoding="UTF-8")
-    #Compare Checksums to test for corrupt data
-    if UDP_Packet[3] == chksum:
-        return True
-    else:
-        return False
+def checkCorrupt(data):
+	UDP_Packet = unpacker.unpack(data)
+	#Create the Checksum for comparison
+	values = (UDP_Packet[0],UDP_Packet[1],UDP_Packet[2])
+	packer = struct.Struct('I I 8s')
+	packed_data = packer.pack(*values)
+	chksum = bytes(hashlib.md5(packed_data).hexdigest())
+	#Compare Checksums to test for corrupt data
+	if UDP_Packet[3] == chksum:
+		return True
+	else:
+		return False
 
 #will return 0 or 1 depending on sequence
-def checkSequence(bytes):
-    UDP_Packet = unpacker.unpack(bytes)
-    return UDP_Packet[0]
+def checkSequence(data):
+	UDP_Packet = unpacker.unpack(data)
+	return UDP_Packet[0]
 
 #Build ACK Packet
 def buildACK(seq):
-    values = (1,seq,'TestData',chksum)
-    UDP_Packet_Data = struct.Struct('I I 8s 32s')
-    UDP_Packet = UDP_Packet_Data.pack(*values)
-    return UDP_Packet
+	values = (1, seq, 'TestData')
+	UDP_Data = struct.Struct('I I 8s')
+	packed_data = UDP_Data.pack(*values)
+	#creates checksum based on values 
+	chksum = bytes(hashlib.md5(packed_data).hexdigest())
+	#now new value with chksum attached
+	values = (1,seq,'TestData',chksum)
+	UDP_Packet_Data = struct.Struct('I I 8s 32s')
+	UDP_Packet = UDP_Packet_Data.pack(*values)
+	return UDP_Packet
 
-def flipper(num):
-	if num == 0:
-		num = 1
-	else:
-		num = 1
-	return num
-
+#OUR IP INFORMATION
 UDP_IP = "127.0.0.1"
 UDP_PORT = 4000
+
+unpacker = struct.Struct('I I 8s 32s')
 
 #Create UDP socket to send and receive
 send_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -55,30 +58,35 @@ currentSeq = 0
 while True:
 
 	#Socket is ready to receive from client
-    data, addr = recv_sock.recvfrom(1024) # buffer size is 1024 bytes
-    print ("Waiting for Client")
+	print ("Waiting for Client")
+	data, addr = recv_sock.recvfrom(1024) 
+	print (addr)
 
-    #packet received, unpack and check sequence bit
-    UDP_Packet = unpacker.unpack(data)
-    packet_seq = checkSequence(data)
+	#packet received, unpack and check sequence bit
+	UDP_Packet = unpacker.unpack(data)
+	packet_seq = checkSequence(data)
 
-    #check if Data is corrupt, will enter if block if NOT corrupt
-    if checkCorrupt(data) == True and packet_seq == currentSeq:
+	#check if Data is corrupt, will enter if block if NOT corrupt
+	if checkCorrupt(data) == True and packet_seq == currentSeq:
 
-    	#Print the received correct data
-        print ("received from: ", addr)
-        print ("received message:", UDP_Packet)
+		#Print the received correct data
+		print ("received from: ", addr)
+		print ("received message:", UDP_Packet)
 
-        #Send ACK packet back with same sequence bit as what was received (we good)
-        sock.sendto(buildACK(currentSeq, dest))
+		#Send ACK packet back with same sequence bit as what was received (we good)
+		recv_sock.sendto(buildACK(currentSeq), addr)
 
-        #Flip the sequence bit, to get ready for the next packet from client
-        if currentSeq == 0:
-        	currentSeq = 1
-        else:
-        	currentSeq = 0
+		#Flip the sequence bit, to get ready for the next packet from client
+		if currentSeq == 0:
+			currentSeq = 1
+		else:
+			currentSeq = 0
 
-    #Corrupt packet or wrong Sequence bit
-    else:
-    	#SEND A ACK with the other sequence bit
-        sock.sendto(buildACK(str(1 - currentSeq)), dest)
+	#Corrupt packet or wrong Sequence bit
+	else:
+		#SEND A ACK with the other sequence bit
+		if currentSeq == 0:
+			currentSeq = 1
+		else:
+			currentSeq = 0
+		recv_sock.sendto(buildACK(currentSeq), addr)
