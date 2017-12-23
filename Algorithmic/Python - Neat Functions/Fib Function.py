4b8020ff2e9a72b0efff3226bfda6391b3a85f72
unknown <atherqureshi07@gmail.com>
2017-12-23 15:12:18 -0500

    added small github programs to repo

---
+++ Algorithmic/Python - Neat Functions/Fib Function.py
@@ -0,0 +1,29 @@
+# returns true if input is in fibonacci sequence, false otherwise. 
+def isInFib(n):
+	if n == 0: return False
+	elif n == 1: return True
+	else:
+		A,B = 1,1
+		FLIP = True
+		while(True):
+			new = A + B
+			if new > n: return False
+			elif new == n: return True
+			else:
+				if(FLIP):
+					A = new 
+					FLIP = not FLIP
+				else:
+					B = new
+					FLIP = not FLIP
+
+# UI main loop 
+if __name__ == '__main__':
+	notExiting = True
+	while(notExiting):
+		n = input( "Enter Number Check if in Fib, else write x to exit: " )
+		if n == 'x': break
+		n = int(n)
+		print(isInFib(n))
+	
+
