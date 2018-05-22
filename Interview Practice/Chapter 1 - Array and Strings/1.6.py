2a91d408153db3789ccb0928103e8647fb2fc9ee
unknown <atherqureshi07@gmail.com>
2018-05-21 21:16:10 -0400

    completed 1.6, removed 1.5, will figure out later

---
+++ Interview Practice/Chapter 1 - Array and Strings/1.6.py
@@ -0,0 +1,35 @@
+# String Compression: Implement a method to perform basic string compression using the counts
+# of repeated characters. For example, the string aabcccccaaa would become a2blc5a3. If the
+# compressed" string would not become smaller than the original string, your method should return
+# the original string. You can assume the string has only uppercase and lowercase letters (a - z).
+
+def Compress(A):
+	A = A.lower()
+	# make new compressed string
+	comStr = ''
+	i = 0
+	while(i <= len(A)-1):
+		skip = CountLetter(A, i)
+		comStr += A[i]
+		comStr += str(skip)
+		i += skip
+	return comStr
+
+def CountLetter(A, i):
+	letter = A[i]
+	end = False
+	i += 1
+	count = 1
+	while (end != True):
+		if i > len(A)-1:
+			break
+		elif A[i] == letter:
+			count += 1
+			i += 1
+		else:
+			end = True
+	return count
+
+string = 'AAAABBBCDDDDGGGGGGGGG'
+
+print Compress(string)
