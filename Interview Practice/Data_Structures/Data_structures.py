2bb837e9910745bc6fbc250965c17a1a493e7b64
unknown <atherqureshi07@gmail.com>
2018-06-22 23:19:29 -0400

    added data_structures.py, where I implemented Stacks and Queues, using a linkedList. I will use this file to tackle more coding questions. may be bugs...using main.py to test

---
+++ Interview Practice/Data_Structures/Data_structures.py
@@ -0,0 +1,96 @@
+class Node:
+	"""
+	Generic Node Class
+	"""
+	def __init__(self, data):
+		self.val = data
+		self.next = None
+		self.prev = None
+
+class Stack:
+	"""
+		LIFO || push, peek, pop, and isEmpty. 
+		Value is any payload. 
+		Needs node class
+	"""
+	def __init__(self):
+		self.head = Node(None)
+		self.size = 0
+
+	def push(self, data):
+		if self.isEmpty():
+			self.head.val = data
+		else:
+			self.head.next = Node(data)
+			self.head.next.prev = self.head
+			self.head = self.head.next
+		self.size += 1
+
+	def peek(self):
+		if self.isEmpty():
+			raise ValueError('Stack has no elements')
+		else:
+			return self.head.val
+
+	def pop(self):
+		if self.isEmpty():
+			raise ValueError('Stack has no elements')
+		elif self.size == 1:
+			result = self.head.val
+			self.head.val = None
+			self.size -= 1
+			return result
+		else:
+			result = self.head.val
+			self.head = self.head.prev
+			self.head.next = None
+			self.size -= 1
+			return result
+
+	def isEmpty(self):
+		return self.size == 0
+
+class Queue:
+	"""
+		FIFO || implements add, remove, examine, and isEmpty
+	"""
+	def __init__(self):
+		self.head = None
+		self.tail = None
+		self.size = 0
+
+	def isEmpty(self):
+		return self.size == 0
+
+	def add(self, value):
+		newNode = Node(value)
+		if self.isEmpty():
+			self.head = newNode
+			self.tail = newNode
+		else:
+			self.head.next = newNode
+			self.head.next.prev = self.head
+			self.head = newNode
+		self.size += 1
+
+	def remove(self):
+		if self.isEmpty():
+			raise ValueError('Queue has no elements')
+		elif self.size == 1:
+			result = self.tail.val
+			self.head = None
+			self.tail = None
+			self.size -= 1
+			return result
+		else:
+			result = self.tail.val
+			self.tail = self.tail.next
+			self.tail.prev = None
+			self.size -= 1
+			return result
+
+	def examine(self):
+		if self.isEmpty():
+			raise ValueError('Queue has no elements')
+		else:
+			return self.tail.val
---
+++ Interview Practice/Data_Structures/main.py
@@ -0,0 +1,14 @@
+from Data_structures import Stack, Queue
+
+def main():
+	myQueue = Queue()
+	myQueue.add(50)
+	myQueue.add('Test')
+	myQueue.add(50)
+	print myQueue.remove()
+	print myQueue.examine()
+	print myQueue.remove();
+	print myQueue.examine();
+	print myQueue.remove();
+
+if __name__ in '__main__': main()
