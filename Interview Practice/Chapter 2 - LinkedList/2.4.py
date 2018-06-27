220b4c90bb6f469c7fa0745c9950402bed6cb5eb
unknown <atherqureshi07@gmail.com>
2018-06-26 22:29:41 -0400

    partitioned in place in O(n)

--- Interview Practice/Chapter 2 - LinkedList/2.4.py
+++ Interview Practice/Chapter 2 - LinkedList/2.4.py
@@ -4,9 +4,8 @@ from Data_structures import Node
 
 def partitionLinkedList(head, pValue):
     """
-    Will Partiion a new linkedList but not in place,
-    needs to return new head, old head pointer 
-    doesn't seem to change
+    Will Partiion a new linkedList, and 
+    replace values in place
     """
     # Checking if value is in list
     partitionValue = head.findNode(pValue)
@@ -24,19 +23,17 @@ def partitionLinkedList(head, pValue):
         else:
             after.append(p.val)
         p = p.next
-    # Make new list with before then Partition, then after
+    # Make new list with before - pValue - after
     new_list = []
     new_list.extend(before)
     new_list.append(pValue)
     new_list.extend(after)
-    head = Node(0)
-    head.val = new_list[0]
-    p2 = head
-    for i in range(1, len(new_list)):
-        p2.next = Node(0)
-        p2.next.val = new_list[i]
-        p2 = p2.next
-    return head
+    p = head
+    # iterate through linkedlist and change values 
+    for i in range(len(new_list)):
+        p.val = new_list[i]
+        p = p.next
+    return
 
 def main():
     head = Node(10)
@@ -44,7 +41,7 @@ def main():
     head.appendToTail(20,'s')
     head.appendToTail(7654,'s')
     head.printLinkedList()
-    newHead = partitionLinkedList(head, 46)
-    newHead.printLinkedList()
+    partitionLinkedList(head, 46)
+    head.printLinkedList()
 
 if __name__ in '__main__': main()
