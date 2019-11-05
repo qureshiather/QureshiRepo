38624242f74af201e7674c2f1f6777814f7f2607
Ather Qureshi <atherqureshi07@gmail.com>
2019-11-04 22:23:32 -0500

    implemented merge sort in python

--- interview_practice/sorting_algorithms/sorting_algorithms.py
+++ interview_practice/sorting_algorithms/sorting_algorithms.py
@@ -1,55 +1,70 @@
 import time
 
-def bubble_sort(arr):
+def bubble_sort(A):
     """
         Keep swaping adjacent elements, until you don't need
         to swap anymore
     """
     sorted = False
     while sorted != True:
-        for i in range(len(arr)-1):
+        for i in range(len(A)-1):
             swap = False
-            if arr[i+1] < arr[i]:
-                arr[i+1], arr[i] = arr[i], arr[i+1]
+            if A[i+1] < A[i]:
+                A[i+1], A[i] = A[i], A[i+1]
                 swap = True
         if swap == False:
             sorted = True
-    return arr
+    return A
 
 
-def merge_sort(arr):
+def merge_sort(A):
     """
-        Divide the array into twos recursively, and sort the smallest pieces
+        Divide the Aay into twos recursively, and sort the smallest pieces
     """
-    def helper_merge_sort(arr, l, r):
-        left = 0
-        right = len(arr)-1
-        m = len(arr)/2
-        merge_sort(arr, )
+    def merge(left, right):
+        result = []        
+        i = 0
+        k = 0
+        while(i < len(left) and k < len(right)):
+            if left[i] <= right[k]:
+                result.append(left[i])
+                i += 1
+            else:
+                result.append(right[k])
+                k += 1
+        if i > k:
+            result.extend(right[k:])
+        else:
+            result.extend(left[i:])
+        return result
 
+    
+    if len(A) == 1:
+        return A
     l = 0
-    r = len(arr)-1
-    m = (l+r)/2
-    left = helper_merge_sort(arr, l, m)
-    right = helper_merge_sort(arr, m+1, r)
-    pass
+    r = len(A)
+    m = int((l+r)/2)
+    L = A[l:m]
+    R = A[m:r]
+    return merge(merge_sort(L), merge_sort(R))
 
 
-def insertion_sort(arr):
-    if len(arr) == 1:
-        return arr
-    for i in range(1, len(arr)):
-        x = arr[i]
+def insertion_sort(A):
+    if len(A) == 1:
+        return A
+    for i in range(1, len(A)):
+        x = A[i]
         j = i-1
-        while j >= 0 and x < arr[j]:
-            # keep moving elements up up from arr, until x > arr[j]
-            arr[j+1] = arr[j]
+        while j >= 0 and x < A[j]:
+            # keep moving elements up up from A, until x > A[j]
+            A[j+1] = A[j]
             j -= 1
-        arr[j+1] = x
-    return arr
+        A[j+1] = x
+    return A
 
 
 if __name__ in '__main__':
-    A = [4,1,2, 454, 5,3, 20]
+    A = [4, 1, 2, 454, 5, 3, 20]
     print(insertion_sort(A))
     print(bubble_sort(A))
+    print(merge_sort(A))
