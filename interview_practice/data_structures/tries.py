f74e479a2a67d9da8c5d7f08a2d1cc33f1983293
Ather Qureshi <atherqureshi07@gmail.com>
2019-12-12 22:20:24 -0500

    could not finish iterative merge sort, wil revisit

--- interview_practice/data_structures/tries.py
+++ interview_practice/data_structures/tries.py
@@ -1,4 +1,19 @@
+class Node:
+
+    def __init__(self, val=None):
+        self.val = val
+        self.children = []
 
 class tries:
 
-    def __init__()
+    def __init__(self, word_list):
+        self.root = Node()
+        for word in word_list:
+            self.add_word(word)
+        return root
+
+    def find_prefix(self, prefix):
+        
+
+    def add_word(self, word):
+        pass
--- interview_practice/sorting_algorithms/sorting_algorithms.py
+++ interview_practice/sorting_algorithms/sorting_algorithms.py
@@ -17,8 +17,21 @@ def bubble_sort(A):
             sorted = True
     return A
 
+def insertion_sort(A):
+    if len(A) == 1:
+        return A
+    for i in range(1, len(A)):
+        x = A[i]
+        j = i-1
+        while j >= 0 and x < A[j]:
+            # keep moving elements up up from A, until x > A[j]
+            A[j+1] = A[j]
+            j -= 1
+        A[j+1] = x
+    return A
 
-def merge_sort(A):
+
+def merge_sort_recur(A):
     """
         Divide the Aay into twos recursively, and sort the smallest pieces
     """
@@ -45,41 +58,54 @@ def merge_sort(A):
     l = 0
     r = len(A)
     m = int((l+r)/2)
-    L = A[l:m]
-    R = A[m:r]
-    return merge(merge_sort(L), merge_sort(R))
+    return merge(merge_sort_recur(A[l:m]), merge_sort_recur(A[m:r]))
 
 
-def insertion_sort(A):
-    if len(A) == 1:
-        return A
-    for i in range(1, len(A)):
-        x = A[i]
-        j = i-1
-        while j >= 0 and x < A[j]:
-            # keep moving elements up up from A, until x > A[j]
-            A[j+1] = A[j]
-            j -= 1
-        A[j+1] = x
+def merge_sort_iter(A):
+
+    def merge(left, right):
+        result = []        
+        i = 0
+        k = 0
+        while(i < len(left) and k < len(right)):
+            if left[i] <= right[k]:
+                result.append(left[i])
+                i += 1
+            else:
+                result.append(right[k])
+                k += 1
+        if i > k:
+            result.extend(right[k:])
+        else:
+            result.extend(left[i:])
+        return result
+
+    # intially each element is size one
+    subarray_size = 1
+    n = len(A)
+
+    # go until the subbarray size equals n
+    while subarray_size < n:
+        
+        level_merges = n//subarray_size
+        
+        # sort in 1s, then 2s, then 3s.
+        for i in range(0, level_merges):
+            
+            left = A[i*subarray_size:i*subarray_size+subarray_size]
+            right = A[i*subarray_size+subarray_size:i*subarray_size+subarray_size+subarray_size]
+            A[i*subarray_size:i*subarray_size+subarray_size+subarray_size] = merge(left, right)
+            
+        subarray_size *= 2
+    
     return A
 
 
 if __name__ in '__main__':
     
-    A = [random.randint(0, 16453) for _ in range(0, 10)]
+    # A = [random.randint(0, 100) for _ in range(0, 10)]
+    A = [1, 5, 234, 7, 3, 2, 1, 19, 11]
 
     start = time.time()
-    print(insertion_sort(A))
-    print(f'insertion_sort: {time.time()-start}')
-
-    start = time.time()
-    print(bubble_sort(A))
-    print(f'bubble_sort: {time.time()-start}')
-
-    start = time.time()
-    print(merge_sort(A))
-    print(f'merge_sort: {time.time()-start}')
-
-    start = time.time()
-    print(sorted(A))
-    print(f'python_sort: {time.time()-start}')
+    print(merge_sort_iter(A))
+    print(f'merge_sort_iter: {time.time()-start}')
